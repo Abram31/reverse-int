10267afb6fd4af7fccd9eaec5d9fa17717721bfc
Abram31 <a447623993@gmail.com>
2022-01-05 22:02:18 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    if ((n).toString()[0] == '-') {
+        return +(n).toString().slice(1).split('').reverse().join('');
+    } else { return +(n).toString().split('').reverse().join('')}
 }
